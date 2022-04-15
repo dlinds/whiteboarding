b40d530289b0606975474c4e789752a025aada49
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-04-15 08:25:07 -0700

    test in c#, get working

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace whiteboarding
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(GenerateURL("this is a test"));
+
+    }
+    public static string GenerateURL(string input)
+    {
+      string output = "";
+      foreach (char letter in input)
+      {
+
+        if (letter == ' ')
+        {
+          output += "%20";
+        }
+        else
+        {
+          output += letter;
+        }
+      }
+      return output;
+    }
+  }
+}
